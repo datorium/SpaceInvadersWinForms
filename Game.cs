be34560c9f0d5ccd2a296174b2733b0a0ff3f23f
Datorium <hello@datorium.lv>
2021-04-01 20:24:43 +0300

    Can fr=ire bullets

--- Game.cs
+++ Game.cs
@@ -13,6 +13,7 @@ namespace SpaceInvadersWinForms
     public partial class Game : Form
     {
         private Starship starship = null;
+        private Bullet bullet = null;
 
         public Game()
         {
@@ -45,8 +46,14 @@ namespace SpaceInvadersWinForms
             {
                 starship.Left += 5;
             }
+            else if(e.KeyCode == Keys.Space)
+            {
+                bullet = new Bullet();
+                bullet.Left = starship.Left + 19;
+                bullet.Top = starship.Top - bullet.Height;
+                this.Controls.Add(bullet);
+            }
         }
-
     }
 
     public class Starship : PictureBox
@@ -56,6 +63,31 @@ namespace SpaceInvadersWinForms
             this.BackColor = Color.SteelBlue;
             this.Width = 40;
             this.Height = 120;
+        }
+    }
+
+    public class Bullet : PictureBox
+    {
+        private Timer timerMove = null;
+        public Bullet()
+        {
+            this.BackColor = Color.Red;
+            this.Width = 2;
+            this.Height = 20;
+            InitializeTimerMove();
+        }
+
+        private void InitializeTimerMove()
+        {
+            timerMove = new Timer();
+            timerMove.Interval = 20;
+            timerMove.Tick += TimerMove_Tick;
+            timerMove.Start();
+        }
+
+        private void TimerMove_Tick(object sender, EventArgs e)
+        {
+            this.Top -= 1;
         }
     }
 }
